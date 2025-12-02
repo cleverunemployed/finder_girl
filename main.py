8bda97a7e78dcb5bab033fb912759739413f3c4e
cleverunemployed <agonycavarly@gmail.com>
2025-12-02 21:03:44 +0400

    bug fix

---
+++ main.py
@@ -0,0 +1,5 @@
+# Photo girl: /html/body/div[2]/div/div[2]/div/div/section/div/div/div/div/div/div[3]/div/div[1]/div/div/div[2]/div[1]/div/div[1]/div[1]
+# //*[@id="current-card"]/div/div[1]/div[1]/img
+
+# Description girl: /html/body/div[2]/div/div[2]/div/div/section/div/div/div/div/div/div[3]/div/div[2]/div/div/div/section[2]/div[2]/div/div
+# //*[@id="main"]/div/div[3]/div/div[2]/div/div/div/section[2]/div[2]/div/div
